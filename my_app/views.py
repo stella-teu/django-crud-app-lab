1a77e19a40b506b7f47964c7515a95ba883bf0db
StellaTeuscher <stella.teuscher@gmail.com>
2025-05-05 10:34:17 -0400

    setup first url and view

--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class Home(APIView):
+    def get(self, request):
+        content = {'message': 'Welcome to monkey collector API home route'}
+        return Response(content)
